7c9777e1f1ad694633540e16277731ae7156e040
Leonardo de Macedo Sartorello <45469728+leollo98@users.noreply.github.com>
2023-08-29 15:52:23 -0300

    Update beanstalk.tf

--- Infra/beanstalk.tf
+++ Infra/beanstalk.tf
@@ -6,7 +6,7 @@ resource "aws_elastic_beanstalk_application" "aplicacao_beanstalk" {
 resource "aws_elastic_beanstalk_environment" "ambiente_beanstalk" {
   name                = var.ambiente
   application         = aws_elastic_beanstalk_application.aplicacao_beanstalk.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.4.10 running Docker"
+  solution_stack_name = "64bit Amazon Linux 2023 v4.0.0 running Docker"
 
 setting {
     namespace = "aws:autoscaling:launchconfiguration"
